7bed28acc2085141be861d05a4da359ee1f408d5
Dikshant Chauhan <94213659+DkRajput25@users.noreply.github.com>
2026-07-27 23:10:01 +0530

    Add 0035-Search-Insert-Position solution

---
+++ 0035-Search-Insert-Position/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0;
+        int end = nums.length-1;
+        int mid= 0;
+        int ans = nums.length;
+        while(start<=end){
+            mid = (start+end)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start = mid+1;
+            }
+            else{
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+}
