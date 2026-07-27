1a61a3029d1de0999aca35f0bbc6dc42eb372470
Dikshant Chauhan <94213659+DkRajput25@users.noreply.github.com>
2026-07-27 22:43:57 +0530

    Update 1464-Maximum-Product-Of-Two-Elements-In-An-Array solution

--- 1464-Maximum-Product-Of-Two-Elements-In-An-Array/Solution.java
+++ 1464-Maximum-Product-Of-Two-Elements-In-An-Array/Solution.java
@@ -1,9 +1,21 @@
-    public int maxProduct(int[] nums) {
-        // int n = nums.length-1;
-        // Arrays.sort(nums);
-        // return (nums[n]-1)*(nums[n-1]-1);
-        int m =0;
-        int n=0;
-        for(int num : nums){
-            if()
-        }
+1class Solution {
+2    public int maxProduct(int[] nums) {
+3        // int n = nums.length-1;
+4        // Arrays.sort(nums);
+5        // return (nums[n]-1)*(nums[n-1]-1);
+6
+7
+8        int m =0;
+9        int n=0;
+10        for(int num : nums){
+11            if(num>m){
+12                n=m;
+13                m=num;
+14            }
+15            else if(num>n){
+16                n = num;
+17            }
+18        }
+19        return (n-1)*(m-1);
+20    }
+21}
