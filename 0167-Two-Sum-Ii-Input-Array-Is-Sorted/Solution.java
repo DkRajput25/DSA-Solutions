5348ca46d80c24edfdd10211136f839c9928b934
Dikshant Chauhan <94213659+DkRajput25@users.noreply.github.com>
2026-07-27 22:19:06 +0530

    Add 0167-Two-Sum-Ii-Input-Array-Is-Sorted solution

---
+++ 0167-Two-Sum-Ii-Input-Array-Is-Sorted/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int nums[] = new int[2];
+        int left= 0;
+        int right = numbers.length - 1;
+        while(left<right){
+            int sum = numbers[left] + numbers[right];
+            if(sum >target){
+                right--;
+            }
+            else if(sum  < target){
+                left ++;
+            }
+            else{
+                nums[0] = left+1;
+                nums[1]=right+1;
+                break;
+            }
+        }
+        return nums;
+    }
+}
