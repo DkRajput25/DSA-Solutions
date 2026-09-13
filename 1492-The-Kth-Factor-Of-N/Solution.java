9a63dd6e7588b33e8ff8f68f6f5ae413a7403c3e
Dikshant Chauhan <94213659+DkRajput25@users.noreply.github.com>
2026-09-13 16:30:17 +0530

    Update 1492-The-Kth-Factor-Of-N solution

--- 1492-The-Kth-Factor-Of-N/Solution.java
+++ 1492-The-Kth-Factor-Of-N/Solution.java
@@ -1,17 +1,14 @@
-1class Solution {
-2    public int kthFactor(int n, int k) {
-3        List<Integer> list = new ArrayList<>();
-4
-5        for(int i = 1; i <= n; i++) {
-6            if(n % i == 0) {
-7                list.add(i);
-8            }
-9        }
-10
-11        if(k<= list.size()){
-12            return list.get(k-1);
-13        }
-14
-15        return -1;
-16    }
-17}
+class Solution {
+    public int kthFactor(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1 ; i <= Math.sqrt(n) ; i++){
+            if(n%i==0){
+                list.add(i) ;
+            if(i != n/i)list.add(n/i);
+            }
+        }
+        Collections.sort(list) ;
+        if(k>list.size())return -1 ;
+        return list.get(k-1) ;
+    }
+}
